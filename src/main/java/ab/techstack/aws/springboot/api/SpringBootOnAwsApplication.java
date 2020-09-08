e445b3321a181bf9ebe29cbbfc72c85a7ea4ca6c
Amol Bhople <amolbhople@gmail.com>
2020-09-08 13:58:57 +0530

    first commit

---
+++ src/main/java/ab/techstack/aws/springboot/api/SpringBootOnAwsApplication.java
@@ -0,0 +1,13 @@
+package ab.techstack.aws.springboot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOnAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOnAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ab/techstack/aws/springboot/api/SpringBootOnAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package ab.techstack.aws.springboot.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOnAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
